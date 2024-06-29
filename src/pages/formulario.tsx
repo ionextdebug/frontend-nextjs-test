c5e8626cdf770f1bb7a8ff814bec57aedb843189
ionextdebug <focusmpublic@gmail.com>
2024-06-29 12:27:54 -0400

    feat: initial state of the form

--- src/pages/formulario.tsx
+++ src/pages/formulario.tsx
@@ -9,25 +9,48 @@
  * - Lide com os possíveis erros
  */
 
+import { useForm, SubmitHandler } from "react-hook-form"
+
 import styles from '@/styles/formulario.module.css';
 
-export default function Form() {
-	function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-		event.preventDefault();
 
-		console.log('submit');
-	}
+type Inputs = {
+	nome: string
+	email: string
+  }
+
+
+export default function Form() {
+
+	const {
+			register,
+			handleSubmit,
+			watch,
+			formState: { errors },
+		} = useForm<Inputs>();
+
+	const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+	console.log(watch("nome"));
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.content}>
-				<form onSubmit={handleSubmit}>
-					<input type="text" placeholder="Name" />
-					<input type="email" placeholder="E-mail" />
+				<form onSubmit={handleSubmit(onSubmit)}>
+					{/* register your input into the hook by invoking the "register" function */}
+					<input placeholder="Nome" {...register("nome", { required: true })} />
+					{errors.nome && <span>Este campo é obrigatório.</span>}
+
+					{/* include validation with required or other standard HTML validation rules */}
+					<input placeholder="E-mail" {...register("email", { required: true })} />
+					{/* errors will return when field validation fails  */}
+					{errors.email && <span>Este campo é obrigatório.</span>}
+
 
 					<button type="submit" data-type="confirm">
 						Enviar
 					</button>
+					
 				</form>
 			</div>
 		</div>
